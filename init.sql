a213a6d96864f1be3255a5e6bd477748eaccf3f7
Danylo Hotvianskyi <danilgotvyansky@gmail.com>
2023-09-11 22:44:43 +0200

    Add nginx

--- init.sql
+++
@@ -1,5 +0,0 @@
-SET global general_log = 1;
-SET global log_output = 'table';
-GRANT ALL PRIVILEGES ON `test_${DB_NAME}`.* TO '${DB_USER}'@'%';
-GRANT ALL PRIVILEGES ON `${DB_NAME}`.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;
